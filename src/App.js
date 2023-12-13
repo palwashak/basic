68d99dcde1642b3522cc620fc2cbb55835168a6b
RIB\palwasha.khan <palwasha.khan@rib-international.com>
2023-12-13 12:29:57 +1000

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Salesforce Service Cloud";
+  }, []);
   const value = 'Joel';
   return <div>Hello {value}</div>;
 }
