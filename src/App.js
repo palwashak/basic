dbae1bf57b56aec57921ed0aceccb08939778e40
RIB\palwasha.khan <palwasha.khan@rib-international.com>
2023-12-19 09:32:42 +1000

    more changes tp App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import nots from "../res.json";
+import nots from "../src/res.json";
+import { useEffect, useState } from "react";
 
 function App() {
   const [notifications, setNotifications] = useState([]);
@@ -7,11 +8,43 @@ function App() {
     setNotifications(nots);
     document.title = "Salesforce Service Cloud";
   }, []);
+
+  const UserRow = (notification, index) => {
+    return (
+      <tr
+        key={index}
+      >
+        <td>{notification.Id}</td>
+        <td>{notification.IsClosed ? "Closed" : "Open"}</td>
+        <td>{notification.ClosedDate}</td>
+        <td>{notification.CaseNumber}</td>
+        <td>{notification.Status}</td>
+        <td>{notification.Subject}</td>
+        <td>{notification.Description}</td>
+      </tr>
+    );
+  };
+  const userTable = notifications.map((notification, index) =>
+    UserRow(notification, index)
+  );
   const value = "Joel";
   return (
     <div>
       Hello {value}
-      <p>{notifications}</p>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Enquiry Reference Number</th>
+            <th>Is Closed</th>
+            <th>Date Closed</th>
+            <th>Case Number</th>
+            <th>Status</th>
+            <th>Subject</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>{userTable}</tbody>
+      </table>
     </div>
   );
 }
