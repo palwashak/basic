dc48d9033f967dba29d9c518ab12cdd17c1152b1
RIB\palwasha.khan <palwasha.khan@rib-international.com>
2023-12-19 08:54:03 +1000

    added res.json and output in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import nots from "../res.json";
 
 function App() {
+  const [notifications, setNotifications] = useState([]);
   useEffect(() => {
+    setNotifications(nots);
     document.title = "Salesforce Service Cloud";
   }, []);
-  const value = 'Joel';
-  return <div>Hello {value}</div>;
+  const value = "Joel";
+  return (
+    <div>
+      Hello {value}
+      <p>{notifications}</p>
+    </div>
+  );
 }
 
 export default App;
